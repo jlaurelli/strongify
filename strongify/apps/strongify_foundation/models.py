94c5649b2103d92897e028dda936f62380d0d966
James Laurelli <j.m.laurelli@gmail.com>
2015-09-28 23:45:39 -0400

    Start sketching out the primary app models

--- strongify/apps/strongify_foundation/models.py
+++ strongify/apps/strongify_foundation/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+"""
+Program - A set of routines. This encompasses several days worth of workouts
+          and together form a complete routine.
+
+          Programs can have multiple "phases" (basically a subprogram).
+
+Routine - A set of exercises that compose a unit in a program. Acts as a
+          linker between the programs and exercises.
+
+Exercise - An atomic exercise movement. These are independent of a routine or
+           program, and multiples can exist within any routine/program.
+"""
+
+
+class Program(models.Model):
+    name = models.CharField(max_length=200)
+    routine = models.ForeignKey(Routine)
+
+    def __str__(self):
+        return self.name
+
+
+class Routine(models.Model):
+    name = models.CharField(max_length=100)
+    program = models.ForeignKey(Model)
+    exercise = models.ForeignKey(Exercise)
+
+    def __str__(self):
+        return self.name
+
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
