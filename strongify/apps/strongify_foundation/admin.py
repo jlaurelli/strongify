93d8449568b74db2dd865ed7b97020889b8b4d92
James Laurelli <j.m.laurelli@gmail.com>
2015-10-10 16:32:23 -0400

    Add primitive admin views

--- strongify/apps/strongify_foundation/admin.py
+++ strongify/apps/strongify_foundation/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Exercise, Program, Routine
+
+
+class ExerciseAdmin(admin.ModelAdmin):
+    fields = ["name"]
+
+
+class RoutineAdmin(admin.ModelAdmin):
+    fields = ["name", "exercise"]
+
+
+class ProgramAdmin(admin.ModelAdmin):
+    fields = ["name", "routine"]
+
+
+admin.site.register(Exercise, ExerciseAdmin)
+admin.site.register(Program, ProgramAdmin)
+admin.site.register(Routine, RoutineAdmin)
