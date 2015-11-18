dce528baf69551e2acd4a2c725813140f86753bb
James Laurelli <j.m.laurelli@gmail.com>
2015-11-17 23:38:02 -0500

    Begin preliminary views

--- strongify/urls.py
+++ strongify/urls.py
@@ -13,6 +13,7 @@ import strongify.views as views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home, name="home"),
+    url(r'^programs/$', views.program, name="program"),
     # url(r'^', include(router.urls)),
     url(r'^api-auth/',
         include('rest_framework.urls', namespace="rest_framework")),
--- strongify/views.py
+++ strongify/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 import strongify.apps.strongify_foundation.models as models
 
 def home(request):
+    return render(request, "index.html", {})
+
+def program(request):
     programs = models.Program.objects.order_by("name")
     context = {"programs": programs}
-    return render(request, "index.html", context)
+    return render(request, "programs.html", context)
